c18287791e05fc29a737dfad5d7ed7bc6d16381e
Joshua George <joshua.vincent.george@gmail.com>
2023-12-27 23:46:23 -0800

    component  created

---
+++ app/components/Banner.js
@@ -0,0 +1,5 @@
+const Banner = () => {
+	return <h1>THIS IS THE SPLASH BANNER</h1>;
+};
+
+export default Banner;
---
+++ app/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+	return <h1 className="flex justify-center mb-4">BOBA MONSTER LOGO</h1>;
+};
+
+export default Header;
