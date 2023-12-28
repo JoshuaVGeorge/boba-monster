08b7eea3a5090548074f1d4a3f0bfb0750da5267
Joshua George <joshua.vincent.george@gmail.com>
2023-12-27 23:53:50 -0800

    wrapped h1 in div

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,5 +1,9 @@
 const Banner = () => {
-	return <h1>THIS IS THE SPLASH BANNER</h1>;
+	return (
+		<div className="flex justify-center">
+			<h1>THIS IS THE SPLASH BANNER</h1>
+		</div>
+	);
 };
 
 export default Banner;
