3eac3c5b48299b43e75e766ec715f32963c2130b
Joshua George <joshua.vincent.george@gmail.com>
2023-12-27 23:46:41 -0800

    imported component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 const Home = () => {
-	return <h1>hi</h1>;
+	return (
+		<div>
+			<Header />
+			<Banner />
+		</div>
+	);
 };
 
 export default Home;
