276555f8fe0b43a377363f90acfd918ab9373a0f
Joshua George <joshua.vincent.george@gmail.com>
2023-12-28 00:04:38 -0800

    added logo image

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,12 @@
+import logo from "../assets/boba-logo.png";
+import Image from "next/image";
+
 const Header = () => {
-	return <h1 className="flex justify-center mb-4">BOBA MONSTER LOGO</h1>;
+	return (
+		<div className="flex justify-center mb-4">
+			<Image src={logo} alt="logo" className="h-[100px] w-[100px]" />
+		</div>
+	);
 };
 
 export default Header;
