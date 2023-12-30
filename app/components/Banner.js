a2d62f057db8297e9d14f759e24d339e93e46784
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 14:49:39 -0800

    Image component imported

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Banner = () => {
 	return (
 		<div className="flex justify-center">
 			<h1>THIS IS THE SPLASH BANNER</h1>
+			{/* <Image src={wave} /> */}
 		</div>
 	);
 };
