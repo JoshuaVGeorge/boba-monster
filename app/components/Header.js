fec535f060e0cf43cdb900a9804f7b02af9225d7
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 16:47:43 -0800

    removed wave

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const Header = () => {
 	return (
-		<div className="flex flex-col items-center bg-white">
+		<div className="flex justify-center bg-white">
 			<div className="mb-4">
 				<Image src={logo} alt="logo" className="h-[100px] w-[100px]" />
 			</div>
-			<div className="relative top-30 left-0 w-full overflow-hidden">
+			{/* <div className="relative top-30 left-0 w-full overflow-hidden">
 				<svg
 					data-name="Layer 1"
 					xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +17,7 @@ const Header = () => {
 						d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
 						className="relative block fill-tea"></path>
 				</svg>
-			</div>
+			</div> */}
 		</div>
 	);
 };
