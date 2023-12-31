007efa230ff70e728e163efd3b75813454d51860
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 18:53:30 -0800

    created and implemented button global style

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Banner = ({ siteName, link, logo }) => {
 	return (
-		<div className=" flex justify-center items-center bg-pearl text-white w-[50px] h-[50px] rounded-full ">
-			<Image src={logo} width={25} height={25} />
-		</div>
+		<Link href={link} className=" rounded-full">
+			<div className=" flex justify-center items-center bg-pearl text-white button">
+				<Image src={logo} width={25} height={25} />
+			</div>
+		</Link>
 	);
 };
 
