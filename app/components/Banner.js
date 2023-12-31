918fa4ee476ffeb647f73c2d1bb1208b56d02104
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 17:45:21 -0800

    added spacing for banner contents

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,9 +1,9 @@
 import Button from "../components/Button";
 const Banner = () => {
 	return (
-		<div className="bg-tea flex flex-col items-center h-[71vh] md:h-[64vh] lg:h-[60vh]">
+		<div className="bg-tea flex flex-col items-center justify-evenly h-[71vh] md:h-[64vh] lg:h-[60vh]">
 			<p>Serving you soon!</p>
-			<div className="flex items-center ">
+			<div className="w-full flex items-center justify-around">
 				<Button />
 				<Button />
 				<Button />
