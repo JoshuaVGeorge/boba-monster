80e6bff997af51932bf04f21fab2c0a7c87b7dbd
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 19:20:44 -0800

    added hover behavior for buttons

--- app/components/Button.js
+++ app/components/Button.js
@@ -3,11 +3,12 @@ import Link from "next/link";
 
 const Banner = ({ siteName, link, logo }) => {
 	return (
-		<Link href={link} className=" rounded-full">
-			<div className=" flex justify-center items-center bg-pearl text-white button">
-				<Image src={logo} width={25} height={25} />
-			</div>
-		</Link>
+		// <Link href={link} className=" rounded-full">
+
+		// </Link>
+		<div className=" flex justify-center items-center bg-pearl text-white button hover:animate-wiggle hover:cursor-pointer active:bg-black">
+			<Image src={logo} width={25} height={25} />
+		</div>
 	);
 };
 
