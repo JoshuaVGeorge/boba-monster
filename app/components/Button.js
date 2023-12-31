7c6be8a05b2fd75320e7ada7b47aca2c8c54b4df
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 19:55:19 -0800

    added hover color

--- app/components/Button.js
+++ app/components/Button.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Button = ({ link, logo }) => {
 	return (
 		<Link href={link} className=" rounded-full">
-			<div className=" flex justify-center items-center bg-pearl text-white button hover:animate-wiggle hover:cursor-pointer active:bg-black">
+			<div className=" flex justify-center items-center bg-pearl text-white button hover:animate-wiggle hover:cursor-pointer hover:bg-white active:bg-black">
 				<Image src={logo} width={25} height={25} />
 			</div>
 		</Link>
