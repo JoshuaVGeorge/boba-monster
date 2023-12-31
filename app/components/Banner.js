c04234623a2a14184791d0588ec87c62bbb19b28
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 17:15:38 -0800

    imported button to banner

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,5 +1,16 @@
+import Button from "../components/Button";
 const Banner = () => {
-	return <div className="bg-tea flex w-full">Serving you soon!</div>;
+	return (
+		<div className="bg-tea flex flex-col items-center ">
+			<p>Serving you soon!</p>
+			<div className="flex items-center ">
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
