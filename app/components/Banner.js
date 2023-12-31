7c8d769ce689d2afcf74b7b3ad0721fab2c53fb1
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 19:42:59 -0800

    created banner component

---
+++ app/components/Banner.js
@@ -0,0 +1,11 @@
+const Banner = () => {
+	return (
+		<div className=" flex justify-center w-full border-t-1 border-b-1 bg-slate-100">
+			<div className="bg-tea rounded-full h-[200px] w-[200px] flex items-center justify-center">
+				<p className="text-xl font-extrabold text-heading">Serving You soon!</p>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
