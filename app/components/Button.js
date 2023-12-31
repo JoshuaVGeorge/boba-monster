e8a21e304e39597522d46fbf13a2325d3cf32d89
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 18:39:02 -0800

    converted button to div with image. passed props

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,5 +1,11 @@
-const Banner = () => {
-	return <button className="bg-pearl text-white p-4 rounded-full">Skip</button>;
+import Image from "next/image";
+
+const Banner = ({ siteName, link, logo }) => {
+	return (
+		<div className=" flex justify-center items-center bg-pearl text-white w-[50px] h-[50px] rounded-full ">
+			<Image src={logo} width={25} height={25} />
+		</div>
+	);
 };
 
 export default Banner;
