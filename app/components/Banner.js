2ae48d6e5b0f07e82049e15585361af4dc676462
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 19:51:33 -0800

    added border color

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,8 +1,8 @@
 const Banner = () => {
 	return (
-		<div className=" flex justify-center w-full border-t-1 border-b-1 bg-slate-100">
-			<div className="bg-tea rounded-full h-[200px] w-[200px] flex items-center justify-center">
-				<p className="text-xl font-extrabold text-heading">Serving You soon!</p>
+		<div className=" flex justify-center w-full border-t-2 border-b-2  border-[#d6b283] bg-slate-100">
+			<div className="bg-tea rounded-full h-[200px] w-[200px] flex items-center justify-center border-2 border-[#d6b283]">
+				<p className="text-xl font-extrabold text-heading">Serving You Soon!</p>
 			</div>
 		</div>
 	);
