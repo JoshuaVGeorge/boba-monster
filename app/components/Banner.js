08710b855d2d25716282bdeae0ace0bfac81eb8f
Joshua George <joshua.vincent.george@gmail.com>
2023-12-30 16:11:29 -0800

    removed svg

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,10 +1,10 @@
+import wave from "../assets/wave.svg";
 import Image from "next/image";
 
 const Banner = () => {
 	return (
-		<div className="flex justify-center">
-			<h1>THIS IS THE SPLASH BANNER</h1>
-			{/* <Image src={wave} /> */}
+		<div className="bg-tea">
+			<div>Hello world</div>
 		</div>
 	);
 };
